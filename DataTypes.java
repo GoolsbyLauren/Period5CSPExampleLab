c33accd72e3786674e32d32ee8e72b981d3d5042
LaurenG <lauren.goolsby@vtsd.com>
2022-09-09 12:27:22 -0400

    First Class Lesson - data type update

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+// Write Whatever I want - comment section
+/*
+    Multi-Line comment syntax.
+
+    Nothing in here will be executed (ran)
+
+ */
+public class DataTypes {
+
+    // Variable syntax: dataType varName = value;
+    int janeFavInt = 5;
+    double snicker = 7.5;
+    boolean likeDogs = true;
+}
--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,2 +1,5 @@
 public class MyFirstProgram {
-}abcdefg
+    public static void main(String args[]){
+        System.out.print("Hello World!");
+    }
+}
