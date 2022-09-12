2c55555ba439340c43790a3f0e292f9ebd51905f
LaurenG <lauren.goolsby@vtsd.com>
2022-09-12 11:43:46 -0400

    First Class Lesson - data type update

--- DataTypes.java
+++ DataTypes.java
@@ -6,9 +6,19 @@
 
  */
 public class DataTypes {
+    public static void main(String args[]){
+        // Variable syntax: dataType varName = value;
+        int janeFavInt = 5;
+        double snicker = 7.5;
+        boolean likeDogs = true;
 
-    // Variable syntax: dataType varName = value;
-    int janeFavInt = 5;
-    double snicker = 7.5;
-    boolean likeDogs = true;
+        System.out.println("Janes Favorite Integer is: " + janeFavInt + " and the class snicker was " + snicker);
+
+        int n1 = 1;
+        int n2 = 2;
+        System.out.print(n1 + n2 / n1);
+
+
+    }
+
 }
